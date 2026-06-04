fac4594a0c8d264bba25f0f226be980b0f49a15d
Guilherme <guiidias11@gmail.com>
2026-06-04 21:05:26 +0000

    commit: Alterado o public IactionResult index para Sobre.

--- PrimeiroProjetoMVC/Controllers/AlunoController.cs
+++ PrimeiroProjetoMVC/Controllers/AlunoController.cs
@@ -1,4 +1,6 @@
+using System.ComponentModel.Design;
 using System.Diagnostics;
+using System.Reflection.PortableExecutable;
 using Microsoft.AspNetCore.Mvc;
 
 namespace PrimeiroProjetoMVC.Controllers;
@@ -15,6 +17,12 @@ public class AlunoController : Controller
     public IActionResult detalhes(int id)
     {
         ViewBag.Alunoid = id;
+        return View();
+    }
+
+    public IActionResult Sobre()
+    {
+        ViewBag.Sobre = "ASP.NET Core MVC Criado pela Microsoft";
         return View();
     }
 }
